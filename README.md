14453cd0c4b19bb0a8faab019178a68133e9786f
AdrielDev <adrielalmeidap@gmail.com>
2021-05-17 21:04:54 -0300

    First exercise done and organized files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-exercises udacity flutter
+# Exercises Udacity Flutter
+
+## Purposes
+- Armazenar exercícios realizados durante o curso udacity.
+
+## Details
+- Course know Flutter
+- By: https://classroom.udacity.com/courses/ud905
---
+++ exercise1/hello_rectangle/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HelloRectangle(),
+    );
+  }
+}
+
+class HelloRectangle extends StatelessWidget {
+  const HelloRectangle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello Rectangle"),
+      ),
+      body: Center(
+        child: Container(
+          height: 400,
+          width: 250,
+          color: Colors.greenAccent,
+          child: Center(
+            child: Text(
+              "Hello",
+              style: TextStyle(fontSize: 40),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
