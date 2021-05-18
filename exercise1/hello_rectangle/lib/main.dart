451ddfccbd8a3883fb4fc931588279d1cd5a85a1
AdrielDev <adrielalmeidap@gmail.com>
2021-05-17 21:19:00 -0300

    Solved problem: changed title of page.

--- exercise1/hello_rectangle/lib/main.dart
+++ exercise1/hello_rectangle/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hello Rectangle',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
